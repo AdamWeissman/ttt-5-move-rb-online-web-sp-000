ec8dca06619bdc40fb12a1ab37144c21d84f3a17
Adam Weissman <adam@comedytragedyepic.com>
2019-09-03 02:20:29 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ def input_to_index(user_input)
   index = user_input.to_i - 1
 end
 
-def move(array, index, value)
+def move(array, index, value = "X")
   array[input_to_index(index)] = value
 end
